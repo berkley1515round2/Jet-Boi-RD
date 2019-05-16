d9baf418c7ae849fb062b9a6f214f9c7eeb657e0
Berkley Fair <berkley1515@gmail.com>
2019-05-16 09:01:27 -0400

    ggh

--- Jet Boi RD/Screens/ShopScreen.Designer.cs
+++ Jet Boi RD/Screens/ShopScreen.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ShopScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ShopScreen";
+            this.Size = new System.Drawing.Size(1435, 641);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
