e7dc6603acd8e5855b3fabf737e0e343f1bc1106
Carter Neeb <cartneeb996@ed.amdsb.ca>
2019-05-16 08:58:45 -0400

    hg

--- Jet Boi RD/Screens/GameScreen.cs
+++ Jet Boi RD/Screens/GameScreen.cs
@@ -32,23 +32,56 @@ namespace Jet_Boi_RD.Screens
         Random r = new Random();
         List<Classes.laser> lasers = new List<Classes.laser>();
         List<Classes.coin> coins = new List<Classes.coin>();
-        public static int backgroundMoveSpd = 4;
+        public static int backgroundMoveSpd = 8;
         Classes.laser laserToRemove;
         Classes.coin coinToRemove;
         public static int coinScore = 0;
-
+        int coinChance = 15;
+        bool endGame = false;
+        int bounce;
+        bool bounceUp = true;
         #endregion
-
         public GameScreen()
         {
             InitializeComponent();
 
 
         }
+        public void GameOver()
+        {
+        }
 
         private void GameTimer_Tick(object sender, EventArgs e)
         {
             tick++;
+            if (endGame && tick % 30 == 0)
+            {
+                if (backgroundMoveSpd > 0) backgroundMoveSpd--;
+                else GameOver();
+            }
+            if (endGame && player.y > this.Height - pheight - 50)
+            {
+                bounceUp = true;
+                bounce /= 2;
+                
+            }
+
+             if (player.y > bounce && bounceUp && endGame)
+            {
+                player.y -= 10;
+                player.hb.Y -= 10;
+                airDownFrames = 0;
+            }
+            else
+            {
+                bounceUp = false;
+                
+            }
+
+            if (r.Next(0, 101) < coinChance && tick % 120 == 0)
+            {
+                generateCoin(r.Next(0, 3), r.Next(100, this.Height - 100));
+            }
             if (tick == 180)
             {
                 lasers.Add(new Classes.laser(this.Width, 10, midLaser, laserWidth));
@@ -57,13 +90,14 @@ namespace Jet_Boi_RD.Screens
             {
                 generateLaser(player.y);
             }
-
-            if (tick % 180 == 0)
+            if (tick % 1200 == 0 && coinChance < 35) coinChance += 5;
+            if (tick % 720 == 0)
             {
-                generateCoin(r.Next(0, 1), 100);
+
                 if (backgroundMoveSpd < 20)
                 {
                     backgroundMoveSpd += 2;
+
                 }
 
                 if (timeBtwnLasers > 30) timeBtwnLasers -= 15;
@@ -102,16 +136,17 @@ namespace Jet_Boi_RD.Screens
             }
             if (jumping)
             {
-                player.y -= 15;
-                player.hb.Y -= 15;
+                player.y -= 30;
+                player.hb.Y -= 30;
                 jumping = false;
             }
+
             Refresh();
         }
 
         private void GameScreen_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Space)
+            if (e.KeyCode == Keys.Space && !endGame)
             {
                 if (grounded) // boost
                 {
@@ -136,26 +171,39 @@ namespace Jet_Boi_RD.Screens
                 l.move();
                 e.Graphics.DrawRectangle(playerPen, l.hb);
                 if (l.hb.Right <= 0) laserToRemove = l;
+                if (player.hb.IntersectsWith(l.hb))
+                {
+                    //GameOver();
+                    if (!endGame)
+                    {
+                        endGame = true;
+                        bounce = (this.Height - player.y) / 2;
+                    }
+                }
             }
+            List<Classes.coin> delete = new List<Classes.coin>();
             foreach (Classes.coin c in coins)
             {
-                c.move();
-                e.Graphics.FillEllipse(coinBrush, c.hb);
-                if (c.hb.Right <= 0) coinToRemove = c;
-
                 if (c.hb.IntersectsWith(player.hb))
                 {
                     coinScore++;
-                    coinToRemove = c;
-                    break;
+                    //coinToRemove = c;
+                    delete.Add(c);
                 }
-               
-                    
-                
+                else
+                {
+                    c.move();
+                    e.Graphics.FillEllipse(coinBrush, c.hb);
+                    if (c.hb.Right <= 0)
+                    {
+                        //coinToRemove = c;
+                        delete.Add(c);
+                    }
+                }
             }
             e.Graphics.DrawString("coins " + coinScore, Font, new SolidBrush(Color.White), 0, 10);
             RemoveLaser(laserToRemove);
-            RemoveCoin(coinToRemove);
+            RemoveCoin(delete);
             coinToRemove = null;
             laserToRemove = null;
         }
@@ -167,6 +215,13 @@ namespace Jet_Boi_RD.Screens
         public void RemoveCoin(Classes.coin l)
         {
             coins.Remove(l);
+        }
+        public void RemoveCoin(List<Classes.coin> l)
+        {
+            foreach (Classes.coin c in l)
+            {
+                coins.Remove(c);
+            }
         }
 
         private void GameScreen_Load(object sender, EventArgs e)
@@ -200,7 +255,7 @@ namespace Jet_Boi_RD.Screens
                     for (int x = 0; x > -5; x--)
                     {
 
-                        c.Add(new Classes.coin(this.Width - (x-1) * 10, pos + x *10));
+                        c.Add(new Classes.coin(this.Width - (x - 1) * 10, pos + x * 10));
 
                     }
                     break;
@@ -209,7 +264,7 @@ namespace Jet_Boi_RD.Screens
                     n = r.Next(20, 100);
                     for (int x = 0; x < 18; x++)
                     {
-                        c.Add(new Classes.coin(this.Width + x * n, pos + n*(float)Math.Sin(45 * x *(Math.PI / 180))));
+                        c.Add(new Classes.coin(this.Width + x * n, pos + n * (float)Math.Sin(45 * x * (Math.PI / 180))));
                     }
                     break;
                 case 3:
