e86d8c51c6f21720c01c05aeecf023903386422d
Carter Neeb <cartneeb996@ed.amdsb.ca>
2019-06-03 09:08:13 -0400

    j

--- Jet Boi RD/Screens/ShopScreen.cs
+++ Jet Boi RD/Screens/ShopScreen.cs
@@ -19,9 +19,27 @@ namespace Jet_Boi_RD.Screens
         public ShopScreen()
         {
             InitializeComponent();
-            teleporterButton.Enabled = !GameScreen.mechs["teleporter"];
-            gravitySuitButton.Enabled = !GameScreen.mechs["gravity"];
-            hogButton.Enabled = !GameScreen.mechs["superJump"];
+            teleporterInfoLabel.Visible = !GameScreen.mechs["teleporter"];
+            teleporterButton.Visible = !GameScreen.mechs["teleporter"];
+            teleportPriceLabel.Visible = !GameScreen.mechs["teleporter"];
+
+            gravitySuitButton.Visible = !GameScreen.mechs["gravity"];
+            gravSuitPriceLabel.Visible = !GameScreen.mechs["gravity"];
+            
+            hogButton.Visible = !GameScreen.mechs["superJump"];
+            hogPriceLabel.Visible = !GameScreen.mechs["superJump"];
+
+            gravityBoiButton.Visible = !GameScreen.upgrades["ironBoi"];
+            gravBoiPriceLabel.Visible = !GameScreen.upgrades["ironBoi"];
+
+            airBoiButton.Visible = !GameScreen.upgrades["jumpBoost"];
+            airBoiPriceLabel.Visible = !GameScreen.upgrades["jumpBoost"];
+
+            xRayBoiButton.Visible = !GameScreen.upgrades["xray"];
+            xRayPriceLabel.Visible = !GameScreen.upgrades["xray"];
+
+            jammerBoiButton.Visible = !GameScreen.upgrades["jammer"];
+            jamerPriceLabel.Visible = !GameScreen.upgrades["jammer"];
 
 
 
@@ -36,6 +54,8 @@ namespace Jet_Boi_RD.Screens
                 GameScreen.coinScore -= 500;
                 GameScreen.mechs["teleporter"] = true;
                 teleporterButton.Enabled = false;
+                teleporterButton.Visible = false;
+                teleportPriceLabel.Visible = false;
                 GameScreen.xmlSave();
             }
         }
@@ -47,6 +67,8 @@ namespace Jet_Boi_RD.Screens
                 GameScreen.coinScore -= 500;
                 GameScreen.mechs["gravity"] = true;
                 gravitySuitButton.Enabled = false;
+                gravitySuitButton.Visible = false;
+                gravSuitPriceLabel.Visible = false;
                 GameScreen.xmlSave();
             }
         }
@@ -58,6 +80,8 @@ namespace Jet_Boi_RD.Screens
                 GameScreen.coinScore -= 500;
                 GameScreen.mechs["superJump"] = true;
                 hogButton.Enabled = false;
+                hogButton.Visible = false;
+                hogPriceLabel.Visible = false;
                 GameScreen.xmlSave();
             }
         }
@@ -74,6 +98,58 @@ namespace Jet_Boi_RD.Screens
             {
                 Form1.switchScreen(this, "game");
                 switchS = false;
+            }
+        }
+
+        private void AirBoiButton_Click(object sender, EventArgs e)
+        {
+            if (GameScreen.coinScore >= 200)
+            {
+                GameScreen.coinScore -= 200;
+                GameScreen.upgrades["jumpBoost"] = true;
+                airBoiButton.Enabled = false;
+                airBoiButton.Visible = false;
+                airBoiPriceLabel.Visible = false;
+                GameScreen.xmlSave();
+            }
+        }
+
+        private void GravityBoiButton_Click(object sender, EventArgs e)
+        {
+            if (GameScreen.coinScore >= 300)
+            {
+                GameScreen.coinScore -= 300;
+                GameScreen.upgrades["ironBoi"] = true;
+                gravityBoiButton.Enabled = false;
+                gravityBoiButton.Visible = false;
+                gravBoiPriceLabel.Visible = false;
+                GameScreen.xmlSave();
+            }
+        }
+
+        private void XRayBoiButton_Click(object sender, EventArgs e)
+        {
+            if (GameScreen.coinScore >= 400)
+            {
+                GameScreen.coinScore -= 400;
+                GameScreen.upgrades["xray"] = true;
+                xRayBoiButton.Enabled = false;
+                xRayBoiButton.Visible = false;
+                xRayPriceLabel.Visible = false;
+                GameScreen.xmlSave();
+            }
+        }
+
+        private void JammerBoiButton_Click(object sender, EventArgs e)
+        {
+            if (GameScreen.coinScore >= 500)
+            {
+                GameScreen.coinScore -= 500;
+                GameScreen.upgrades["jammer"] = true;
+                jammerBoiButton.Enabled = false;
+                jammerBoiButton.Visible = false;
+                jamerPriceLabel.Visible = false;
+                GameScreen.xmlSave();
             }
         }
     }
